f8cb2d16ab360b3451d6bb11bc138c8621a61573
Bella Roberson <162742673+brobers0339@users.noreply.github.com>
2025-10-28 17:59:49 -0400

    Add docstring comments

--- apputil.py
+++ apputil.py
@@ -10,6 +10,22 @@ class GroupEstimate:
         self.group_estimates = None
     
     def fit(self, X: pd.DataFrame, y):
+        '''
+        Fit the GroupEstimate model through calculating group-wise estimates.
+        
+        Parameters
+        -----------
+        X : pd.DataFrame
+            A DataFrame containing only categorical features. Each column represents a category.
+        y : array
+            A 1-D array of target values corresponding to each row in the X variable.
+            
+        Returns
+        -------
+        self : GroupEstimate
+            The fitted model with group estimates is stored in self.group_estimates variable.
+        '''
+        
         df = X.copy()
         df['y'] = y
         agg_type = 'mean' if self.estimate == 'mean' else 'median'
@@ -21,6 +37,21 @@ class GroupEstimate:
         return self
 
     def predict(self, X):
+        '''
+        Use the fitted GroupEstimates model to predict the estimates for the categorical y column.
+        
+        Parameters
+        -----------
+        X : GroupEstimate object
+            A GroupEstimate instance containing a data frame with estimates
+            (mean or median depending on estimate type) for each categorical variable.
+
+        Returns
+        -------
+        merged_df : numpy array
+            A numpy array from the merged data frame that contains the y categorical
+            variables and their estimates.
+        '''
         X = pd.DataFrame(X, columns=self.group_estimates.columns[:-1])
         merged_df = X.merge(self.group_estimates, how='left',on=list(X.columns))
         
