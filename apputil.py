cd0f3d6a4f87aea0a65fccdc383a1891edd87bd2
Bella Roberson <162742673+brobers0339@users.noreply.github.com>
2025-10-28 17:37:28 -0400

    First test for apputil exercise 1 code

--- apputil.py
+++ apputil.py
@@ -1,12 +1,24 @@
 import pandas as pd
 
 
-def GroupEstimate(object):
-    def __init__(self, estimate):
+class GroupEstimate:
+    def __init__(self, estimate='mean'):
         self.estimate = estimate
+        self.group_estimates = None
     
-    def fit(self, X, y):
-        return None
+    def fit(self, X: pd.DataFrame, y):
+        df = X.copy()
+        df['y'] = y
+        agg_type = 'mean' if self.estimate == 'mean' else 'median'
+        self.group_estimates = \
+            df.groupby(list(X.columns))['y'] \
+            .agg(agg_type) \
+            .reset_index()
+        
+        return self
 
     def predict(self, X):
-        return None
+        X = pd.DataFrame(X, columns=self.group_estimates.columns[:-1])
+        merged_df = X.merge(self.group_estimates, how='left',on=list(X.columns))
+        
+        return merged_df['y'].tolist()
